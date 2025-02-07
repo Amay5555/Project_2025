51e050cb88192e24c972024afb73eef36792ebef
Amay Thakur <thakuramay007@gmail.com>
2025-02-07 12:29:27 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import logging
 from app import create_app
 from waitress import serve
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("waitress")
+
+logging.basicConfig(level=logging.INFO)  
+logger = logging.getLogger(__name__)  
 
 app = create_app()
 
 if __name__ == "__main__":
-    host = "0.0.0.0" 
+    host = "0.0.0.0"
     port = 5000
-    logger.info(f"Serving on http://127.0.0.1:{port}")
+    logger.info(f"Starting app on {host}:{port}")
     serve(app, host=host, port=port, threads=2)
